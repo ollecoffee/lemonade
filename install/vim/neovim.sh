c142812f5a3753f3dfab6e482918d8571bdb2a08
Olle Gynther - Zillén <zillen001@C02RQ1ZLG8WL.local>
2017-09-20 16:39:21 +0200

    2lol

--- install/vim/neovim.sh
+++ install/vim/neovim.sh
@@ -3,4 +3,4 @@
 # symlink Neovim files
 
 # symlink
-ln -fs "$HOME/lemonade/conf/neovim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -fs "$HOME/lemonade/conf/vim/init.vim" "$HOME/.config/nvim/init.vim"
