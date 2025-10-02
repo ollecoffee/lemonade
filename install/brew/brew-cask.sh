03f1d750921aff3949e663e1b6778074c2036b40
Olle Korkmaz Zillén <ollekz@icloud.com>
2025-10-02 10:47:18 +0200

    Update brew-cask.sh

--- install/brew/brew-cask.sh
+++ install/brew/brew-cask.sh
@@ -1,38 +1,22 @@
 # Installs brew cask which is used to install applications
 
-# packages to install
-java=(
-  intellij-idea                                 # IDE when you cba terminal
-)
-
 apps=(
-  aerial                                        # screensaver
-  alfred                                        # utils
-  alacritty                                     # superb terminal
-  bartender                                     # bar upgrades
-  datweatherdoe                                 # weather in bar
-  dbeaver-community                             # db
   discord                                       # communication
   firefox                                       # firefox
   google-chrome                                 # chrome
-  stats                                         # menu bar coolness
+  ghostty                                       # default terminal
   insomnia                                      # dev (better postman)
   iterm2                                        # standard terminal
-  latest                                        # always use bleeding edge
-  libreoffice                                   # opensource office
+  latest                                        # always use bleeding edg
   miro                                          # agile
-  opera                                         # browser
   overkill                                      # don't open shiiiii
-  postman                                       # dev
-  robo-3t                                       # db
   signal                                        # communication
-  skype                                         # communication
   sloth                                         # see trafic
   spotify                                       # utils
   sublime-text                                  # text editor
   vlc                                           # utils
+  wezterm                                       # cooler iterm
 )
 
 # install packages
 brew install --cask "${apps[@]}"
-brew install --cask "${java[@]}"
